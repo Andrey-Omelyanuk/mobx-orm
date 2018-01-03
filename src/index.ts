3f15dae817d2f8fca6a9f2965f854b2fc386f898
andrey omelyanuk <omelyanukandrey@gmail.com>
2018-01-03 03:34:11 +0300

    savepoint

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,31 @@ import "reflect-metadata"
 // Store
 
 let store = {
+	// models {
+	//  <class>: {
+	//    fileds: ...
+	//    objects: {
+	//      <id>: <object>
+	//    }
+	//  }
+	// }
 	models: {},
-	history: []
+	transaction: {
+		history: [],
+		root: null,
+		current: null
+	}
 };
 
 
-function history(model_name: string, property_key: string, old_value, new_value) {
-	store.history.push([model_name, property_key, old_value, new_value]);
+function history(model_name: string, id, obj) {
+	if (store.transaction.current == null) {
+		throw new Error(`You cannot change data without transaction.`);
+	}
+	let history = store.transaction.current.history;
+	if (!history[model_name]    ) { history[model_name] = {}; }
+	if (!history[model_name][id]) { history[model_name][id] = {}; }
+	Object.assign(history[model_name][id], obj);
 }
 
 function fixModelInStore(model_name) {
@@ -25,18 +43,75 @@ function getType(target, key){
 
 
 // ------------------------------------------------------------------------------------------------
+// Transaction
+
+class Transaction {
+	private name;
+	private children: Transaction[] = [];
+	private parent: Transaction = null;
+	// history = {
+	//    <class>: {
+	//      <id>: new_value
+	// }
+	public  history = {};
+
+	constructor(name?: string) {
+		this.name = name;
+		this.parent = store.transaction.current;
+		if (this.parent == null) {
+			store.transaction.history.push(this);
+		}
+		else {
+			this.parent.children.push(this);
+		}
+		store.transaction.current = this;
+	}
+
+	public commit() {
+		// выполнение события transaction_commit на каждом объекте который изменился во время транзакции
+		for (let cls_name in this.history) {
+			for (let obj of this.history[cls_name]) {
+				obj.transaction_commit();
+			}
+		}
+		store.transaction.current = this.parent;
+	}
+
+	public rollback() {
+		// откатываем изменения по истории что была на этой транзакции
+	}
+
+	public checkpoint() {
+		// все что успели сделать, оборачиваем в новую транзакцию и делаем commit для этой новой транзакции
+	}
+}
+
+
+function transaction() {
+
+}
+
+// ------------------------------------------------------------------------------------------------
 // Model
 
 class Model {
 	async save()   {
+		let t = new Transaction('save');
 		let model_name = this.constructor.name;
 		let id = Math.random().toString(36).substring(7);
-		history(model_name, 'id', (<any>this).__data['id'], id);
 		(<any>this).__data['id'] = id;
 		store.models[model_name].objects[id] = this;
+		history(model_name, id, (<any>this).__data);
+		t.commit();
 	}
 	async delete() { (<any>this).id = undefined; }
 	static filter (where?, orderBy? : string[], limit? : number, offset? : number) : any {
+	}
+
+	public transaction_commit() {
+		// транзакция почти завершина, этот this объект уставствовал в ней
+		// и если нужно как то на это отреагировать, то реагировать нужно сдесь
+		// например отправить запрос на сервер
 	}
 }
 
@@ -96,7 +171,7 @@ function field(cls: any, field_key: string) {
 		Object.defineProperty (obj, field_key, {
 			get: () => obj.__data[field_key],
 			set: (new_value) => {
-				history(model_name, field_key, obj.__data[field_key], new_value);
+				history(model_name, obj.__data.id, ({}[field_key] = new_value) );
 				obj.__data[field_key] = new_value;
 			}
 		});
@@ -142,7 +217,7 @@ function foreign(key?: string) {
 					if (!new_obj.id) {
 						throw new Error(`Your instance should have id`);
 					}
-					history(model_name, field_key, obj.__data[key], new_obj.id);
+					history(model_name, obj.__data.id, ({}[field_key] = new_obj.id) );
 					obj.__data[key] = new_obj.id;
 				}
 			});
