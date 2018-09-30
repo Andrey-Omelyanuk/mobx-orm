950b45c48f54dd23298aaa618b0fd125879c0121
andrey omelyanuk <omelyanukandrey@gmail.com>
2018-10-01 01:00:54 +0300

    improved tests

--- src/fields/foreign.spec.ts
+++ src/fields/foreign.spec.ts
@@ -31,49 +31,40 @@ describe('Foreign', () => {
 		let a1 = new A({test: 1}); await a1.save()
 		let a2 = new A({test: 2}); await a2.save()
 
-		// default
-		let b = new B()
-		expect(b.a1_id).toBeNull()
-		expect(b.a2_id).toBeNull()
-		expect(b.a1).toBeNull()
-		expect(b.a2).toBeNull()
-
+		let b = new B(); 	expect(b.a1_id).toBeNull()
+											expect(b.a2_id).toBeNull()
+											expect(b.a1   ).toBeNull()
+											expect(b.a2   ).toBeNull()
 		// set id
-		b.a1_id = a2.id
-		expect(b.a1_id).toBe(a2.id)
-		expect(b.a1   ).toBe(a2)
+		b.a1_id = a2.id;  expect(b.a1_id).toBe(a2.id)
+											expect(b.a1   ).toBe(a2)
 		// set foreign
-	  b.a1 = a1
-		expect(b.a1_id).toBe(a1.id)
-		expect(b.a1   ).toBe(a1)
-
+	  b.a1 = a1;			  expect(b.a1_id).toBe(a1.id)
+											expect(b.a1   ).toBe(a1)
 		// set foreign
-		b.a2 = a2
-		expect(b.a2_id).toBe(a2.id)
-		expect(b.a2   ).toBe(a2)
+		b.a2 = a2;			  expect(b.a2_id).toBe(a2.id)
+											expect(b.a2   ).toBe(a2)
 		// set id
-		b.a2_id = null
-		expect(b.a2_id).toBeNull()
-		expect(b.a2   ).toBeNull()
-		b.a2_id = a2.id
-		expect(b.a2_id).toBe(a2.id)
-		expect(b.a2   ).toBe(a2)
-	  // проверяем что на а1 не изменилась
-		expect(b.a1_id).toBe(a1.id)
-		expect(b.a1   ).toBe(a1)
+		b.a2_id = null; 	expect(b.a2_id).toBeNull()
+											expect(b.a2   ).toBeNull()
+
+		b.a2_id = a2.id; 	expect(b.a2_id).toBe(a2.id)
+											expect(b.a2   ).toBe(a2)
+	  									// проверяем что на а1 не изменилась
+											expect(b.a1_id).toBe(a1.id)
+											expect(b.a1   ).toBe(a1)
 	})
 
 	it('cross link', async () => {
-
 		let a = new A(); await a.save()
 		let b = new B(); await b.save()
 
 		a.b = b
 		b.a = a
+
 		expect(a.b_id).toBe(b.id)
 		expect(b.a_id).toBe(a.id)
 		expect(a.b).toBe(b)
 		expect(b.a).toBe(a)
-
 	})
 })
--- src/fields/one.spec.ts
+++ src/fields/one.spec.ts
@@ -19,25 +19,22 @@ describe('One', () => {
 	}
 
 	it('...', async ()=> {
-		// // default
+
 		let a = new A({id: 1})
 		let b1 = new B()
 		let b2 = new B()
+
 		expect(a.b).toBeNull()
 
-		b1.a_id = 1
-		expect(a.b).toBe(b1)
+		b1.a_id = 1;  expect(a.b).toBe(b1)
 
-		a.b = null
-		expect(a.b).toBeNull()
-		expect(b1.a_id).toBeNull()
+		a.b = null; 	expect(a.b    ).toBeNull()
+									expect(b1.a_id).toBeNull()
 
-		a.b = b2
-		expect(a.b).toBe(b2)
-		expect(b1.a_id).toBeNull()
-		expect(b2.a_id).toBe(a.id)
+		a.b = b2; 		expect(a.b    ).toBe(b2)
+									expect(b1.a_id).toBeNull()
+									expect(b2.a_id).toBe(a.id)
 
-		expect(() => { b1.a_id = a.id })
-			.toThrow(new Error('Not unique value. (One)'))
+		expect(() => { b1.a_id = a.id }).toThrow(new Error('Not unique value. (One)'))
 	})
 })
