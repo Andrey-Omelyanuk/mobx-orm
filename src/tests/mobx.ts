5157b6c8bffc3442fe06204a23201caa8df48777
andrey omelyanuk <omelyanukandrey@gmail.com>
2018-10-26 03:31:53 +0300

    mobx works!

--- src/tests/mobx.ts
+++ src/tests/mobx.ts
@@ -1,31 +1,68 @@
-import { observable, computed } from 'mobx'
+import { observable, computed, reaction } from 'mobx'
 
 
 describe('Mobx Experiments.', async () => {
 
-
-
 	it('...', async ()=> {
 
-		class A {
-			@observable a1: string = ''
-			@observable a2: string = ''
+		let models = {}
+		observable(models)
 
-			@computed get a(): string { return this.a1 + this.a2 }
+		class Model {
+			@observable id: number
+
+			constructor(init_data?) {
+				let model_name = this.constructor.name
+				if (models[model_name] === undefined) models[model_name] = {}
+
+				let reaction_id = reaction(
+					() => this.id,
+					id => models[model_name][id] = this
+				)
+
+				// set fields from init data
+				if (init_data)
+					for (let field_name in init_data)
+						this[field_name] = init_data[field_name]
+			}
 		}
 
-		class B {
-			@observable b1: string = ''
-			@observable b2: string = ''
+		class A extends Model {
+			@observable x: string
+			@observable y: string
 
-			@computed get b(): string { return this.b1 + this.b2 }
+			@computed get z(): string { return this.x + this.y }
+
+			constructor(init_data?) {
+				super(init_data)
+			}
 		}
 
-		let A1 = new A(); A1.a1 = 'a'
-		let A2 = new A(); A2.a1 = 'a'; A2.a2 = 'aa'
+		class B extends Model{
+			@observable a_id: number
+			@computed get a() : A {
+				return models['A'][this.a_id]
+			}
 
-		expect(A1.a).toBe('a')
-		expect(A2.a).toBe('aaa')
+			constructor(init_data?) {
+				super(init_data)
+			}
+		}
 
+		let a1 = new A({id: 1, x: 'a1'})
+		let a2 = new A({id: 2, x: 'a2', y: 'y'})
+		let b  = new B()
+
+		expect(a1.x).toBe('a1')
+		expect(a2.x).toBe('a2')
+		expect(a2.z).toBe('a2y')
+
+		expect(b.a).toBeUndefined()
+		b.a_id = a1.id
+		expect(b.a).toBe(a1)
+		b.a_id = a2.id
+		expect(b.a).toBe(a2)
+		b.a_id = null
+		expect(b.a).toBeUndefined()
 	})
 })
