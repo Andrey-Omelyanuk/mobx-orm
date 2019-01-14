8d9b88492a4e3fa00d132ba70729d0128a79556d
Andrey Omelyanuk <omelyanukandrey@gmail.com>
2019-01-14 20:23:00 +0300

    added full export and updated mobx

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-// import store  from './store'
-// import Filter from './filter'
-//
-// export {
-// 	store,
-// 	Filter
-// }
-//
+import store            from './store'
+import {Model, model}   from './model'
+import id               from './fields/id'
+import field            from './fields/field'
+import foreign          from './fields/foreign'
+import one              from './fields/one'
+import many             from './fields/many'
+
+export {
+    store,
+    model,
+    Model,
+    id,
+    field,
+    foreign,
+    one,
+    many
+}
+
