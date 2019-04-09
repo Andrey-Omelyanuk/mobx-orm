b9c324480834c69fb15e87fb75c0539e2d7771cd
Andrey Omelyanuk <omelyanukandrey@gmail.com>
2019-04-09 16:20:39 +0300

    smallfix

--- src/model.ts
+++ src/model.ts
@@ -48,7 +48,7 @@ export class Model {
 		}
 	}
 
-	async load(where, order_by, limit, offset) {
+	async load(where = {}, order_by = {}, limit = 0, offset = 0) {
 		let model_name = this.constructor.name
 		let model_description = store.models[model_name]
 		if (model_description.load) 
