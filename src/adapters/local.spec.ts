84630e1c7b3f8b4a2a7a614f8a89be4d09ed361f
Andrey Omelyanuk <omelyanukandrey@gmail.com>
2021-10-04 19:34:14 +0300

    in progress

--- src/adapters/local.spec.ts
+++ src/adapters/local.spec.ts
@@ -35,144 +35,141 @@ describe('Adapter: Local', () => {
         jest.clearAllMocks()
     })
 
-    describe('constructor', () => {
-        it('create a new instance', async ()=> {
-            let adapter = new LocalAdapter(A)
-            expect(adapter.cls).toBe(A)
-            expect(adapter.store_name).toBe('A')
-        })
+    // describe('constructor', () => {
+    //     it('create a new instance', async ()=> {
+    //         let adapter = new LocalAdapter(A)
+    //         expect(adapter.model).toBe(A)
+    //         expect(adapter.store_name).toBe('A')
+    //     })
 
-        it('decorate the model', async ()=> {
-            @local()
-            @model class A extends Model {}
-            expect((<any>A).adapter.cls).toBe(A)
-            expect((<any>A).adapter.store_name).toBe('A')
-        })
-    })
+    //     it('decorate the model', async ()=> {
+    //         @local()
+    //         @model class A extends Model {}
+    //         expect((<any>A).adapter.model).toBe(A)
+    //         expect((<any>A).adapter.store_name).toBe('A')
+    //     })
+    // })
 
     describe('create', () => {
 
         it('create', async ()=> {
-            let a = new A({a: 1})
-            expect(a.id).toBeNull()
-            expect(store['A']).toEqual({})
-            expect(create).toHaveBeenCalledTimes(0)
-            await a.create()
-            expect(a.id).toBe(1)
-            expect(store['A']).toEqual({
-                [A.__id(a)]: a.raw_obj, 
-            })
-            expect(create).toHaveBeenCalledTimes(1)
-            expect(create).toHaveBeenCalledWith(a)
+            let a = new A({a: 1});      expect(a.id).toBeNull()
+                                        expect(store['A']).toEqual({})
+                                        expect(create).toHaveBeenCalledTimes(0)
+            let b = await a.create();   expect(a).toBe(b)
+                                        expect(a.id).toBe(1)
+                                        // expect(store['A']['1']).toEqual(1)
+                                        expect(store['A']).toEqual({
+                                            [a.__id]: a.raw_obj, 
+                                        })
+                                        expect(create).toHaveBeenCalledTimes(1)
+                                        expect(create).toHaveBeenCalledWith(a)
         })
 
-        it('create few objets', async ()=> {
-            let a = new A(); await a.create(); expect(a.id).toBe(1)
-            let b = new A(); await b.create(); expect(b.id).toBe(2)
-            let c = new A(); await c.create(); expect(c.id).toBe(3)
-            expect(create).toHaveBeenCalledTimes(3)
-            expect(store['A']).toEqual({
-                [A.__id(a)]: a.raw_obj, 
-                [A.__id(b)]: b.raw_obj, 
-                [A.__id(c)]: c.raw_obj})
-        })
-    })
-
-    describe('update', () => {
-
-        it('update exist object', async ()=> {
-            let a = new A({a: 1})
-            await a.create()
-
-            expect(a).toMatchObject({id: 1, a: 1, b: undefined})
-            expect(store['A']).toEqual({
-                [A.__id(a)]: a.raw_obj, 
-            })
-            a.b = 'x'
-            await a.update()
-            expect(a).toMatchObject({id: 1, a: 1, b: 'x'})
-            expect(store['A']).toEqual({
-                [A.__id(a)]: a.raw_obj, 
-            })
-            expect(update).toHaveBeenCalledTimes(1)
-            expect(update).toHaveBeenCalledWith(a)
-        })
-
-        // TODO
-        // it('update does not exist object', async ()=> {
+        // it('create few objets', async ()=> {
+        //     let a = new A(); await a.create(); expect(a.id).toBe(1)
+        //     let b = new A(); await b.create(); expect(b.id).toBe(2)
+        //     let c = new A(); await c.create(); expect(c.id).toBe(3)
+        //     expect(create).toHaveBeenCalledTimes(3)
+        //     expect(store['A']).toEqual({
+        //         [A.__id(a)]: a.raw_obj, 
+        //         [A.__id(b)]: b.raw_obj, 
+        //         [A.__id(c)]: c.raw_obj})
         // })
     })
 
-    describe('delete', () => {
-        it('delete', async ()=> {
-            let a = new A({id: 1, a: 'test'})
-            a.create()
+    // describe('update', () => {
 
-            expect(a).toMatchObject({id: 1, a: 'test'})
-            expect(store['A']).toEqual({
-                [A.__id(a)]: a.raw_obj, 
-            })
-            await a.delete()
-            expect(a).toMatchObject({id: null, a: 'test'})
-            expect(store['A']).toEqual({})
-        })
-    })
+    //     it('update exist object', async ()=> {
+    //         let a = new A({a: 1})
+    //         await a.create()
 
-    describe('load', () => {
-    //     // TODO
-    //     it('load', async ()=> {
-    //         @local()
-    //         @model class A extends Model {
-    //             @id    id: number
-    //             @field  a: string
-    //         }
-    //         let adapter: LocalAdapter<A> = (<any>A).adapter
-    //         expect(() => { adapter.load() })
-    //             .toThrow(new Error(`Not implemented`))
+    //         expect(a).toMatchObject({id: 1, a: 1, b: undefined})
+    //         expect(store['A']).toEqual({
+    //             [A.__id(a)]: a.raw_obj, 
+    //         })
+    //         a.b = 'x'
+    //         await a.update()
+    //         expect(a).toMatchObject({id: 1, a: 1, b: 'x'})
+    //         expect(store['A']).toEqual({
+    //             [A.__id(a)]: a.raw_obj, 
+    //         })
+    //         expect(update).toHaveBeenCalledTimes(1)
+    //         expect(update).toHaveBeenCalledWith(a)
     //     })
-    })
 
-    describe('init_local_data', () => {
+    //     // TODO
+    //     // it('update does not exist object', async ()=> {
+    //     // })
+    // })
 
-        it('empty', async ()=> {
-            let store_name = LocalAdapter.getStoreName(A)
-            init_local_data(A, [])
-            expect(store[store_name]).toEqual({})
-        })
+    // describe('delete', () => {
+    //     it('delete', async ()=> {
+    //         let a = new A({id: 1, a: 'test'})
+    //         a.create()
 
-        it('with some data', async ()=> {
-            let store_name = LocalAdapter.getStoreName(A)
-            let data_set = [
-                {id: 1, a: 1, b: 'a', c: true },
-                {id: 2, a: 2, b: 'b', c: false},
-            ]
-            init_local_data(A, data_set)
-            expect(store[store_name][A.__id(data_set[0])]).toMatchObject(data_set[0])
-            expect(store[store_name][A.__id(data_set[1])]).toMatchObject(data_set[1])
-        })
+    //         expect(a).toMatchObject({id: 1, a: 'test'})
+    //         expect(store['A']).toEqual({
+    //             [A.__id(a)]: a.raw_obj, 
+    //         })
+    //         await a.delete()
+    //         expect(a).toMatchObject({id: null, a: 'test'})
+    //         expect(store['A']).toEqual({})
+    //     })
+    // })
 
-        it('override data', async ()=> {
-            let store_name = LocalAdapter.getStoreName(A)
-            let data_set_a = [
-                {id: 1, a: 1, b: 'a', c: true },
-                {id: 2, a: 2, b: 'b', c: false},
-            ]
+    // describe('load', () => {
+    // //     // TODO
+    // //     it('load', async ()=> {
+    // //         @local()
+    // //         @model class A extends Model {
+    // //             @id    id: number
+    // //             @field  a: string
+    // //         }
+    // //         let adapter: LocalAdapter<A> = (<any>A).adapter
+    // //         expect(() => { adapter.load() })
+    // //             .toThrow(new Error(`Not implemented`))
+    // //     })
+    // })
 
-            let data_set_b = [
-                {id: 2, a: 2, b: 'b', c: false},
-            ]
+    // describe('init_local_data', () => {
 
-            init_local_data(A, data_set_a)
-            init_local_data(A, data_set_b)
-            expect(store[store_name][A.__id(data_set_b[0])]).toMatchObject(data_set_b[0])
-        })
+    //     it('empty', async ()=> {
+    //         init_local_data(A, [])
+    //         expect(store['A']).toEqual({})
+    //     })
 
-        // TODO
-        // it('error: data has no id', async ()=> {
-        // })
+    //     it('with some data', async ()=> {
+    //         let data_set = [
+    //             {id: 1, a: 1, b: 'a', c: true },
+    //             {id: 2, a: 2, b: 'b', c: false},
+    //         ]
+    //         init_local_data(A, data_set)
+    //         expect(store['A'][A.__id(data_set[0])]).toMatchObject(data_set[0])
+    //         expect(store['A'][A.__id(data_set[1])]).toMatchObject(data_set[1])
+    //     })
 
-        // TODO
-        // it('error: data has duplication id', async ()=> {
-        // })
-    })
+    //     it('override data', async ()=> {
+    //         let data_set_a = [
+    //             {id: 1, a: 1, b: 'a', c: true },
+    //             {id: 2, a: 2, b: 'b', c: false},
+    //         ]
+
+    //         let data_set_b = [
+    //             {id: 2, a: 2, b: 'b', c: false},
+    //         ]
+
+    //         init_local_data(A, data_set_a)
+    //         init_local_data(A, data_set_b)
+    //         expect(store['A'][A.__id(data_set_b[0])]).toMatchObject(data_set_b[0])
+    //     })
+
+    //     // TODO
+    //     // it('error: data has no id', async ()=> {
+    //     // })
+
+    //     // TODO
+    //     // it('error: data has duplication id', async ()=> {
+    //     // })
+    // })
 })
