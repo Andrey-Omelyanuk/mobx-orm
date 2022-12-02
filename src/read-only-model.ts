dea5ec5098e0ec64c39ba0610d9d3843744d668c
Andrey <omelyanukandrey@gmail.com>
2022-12-02 22:14:16 +0300

    fix typo

--- src/read-only-model.ts
+++ src/read-only-model.ts
@@ -5,5 +5,5 @@ export abstract class ReadOnlyModel extends Model {
     async create() { throw(`You cannot create the obj, ${this.model.name} is READ ONLY model`) }
     async update() { throw(`You cannot update the obj, ${this.model.name} is READ ONLY model`) }
     async delete() { throw(`You cannot delete the obj, ${this.model.name} is READ ONLY model`) }
-    async save  () { throw(`You cannot save   the obj, ${this.model.name} is READ ONLY model`) }
+    async save  () { throw(`You cannot save the obj, ${this.model.name} is READ ONLY model`) }
 }
