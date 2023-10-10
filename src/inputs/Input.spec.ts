f616e668a1e571bc74295ded812239eaeaaf14e7
Andrey Omelyanuk <omelyanukandrey@gmai.com>
2023-10-10 13:35:40 +0100

    1.3.11

--- src/inputs/Input.spec.ts
+++ src/inputs/Input.spec.ts
@@ -11,12 +11,12 @@ jest.useFakeTimers()
 
 describe('Input', () => {
 
-    class TestModel extends Model {}
+    class TestModel extends Model { }
     TestModel.__adapter = new BaseTestAdapter(TestModel)
 
-    class TestInput extends Input<string|null|undefined> {
-        serialize(value?: string): string|null|undefined { return }
-        deserialize(value?: string|null|undefined): string { return ''}
+    class TestInput extends Input<string | null | undefined> {
+        serialize(value?: string): string | null | undefined { return }
+        deserialize(value?: string | null | undefined): string { return '' }
     }
     describe('constructor', () => {
         it('no value and options', async () => {
@@ -28,7 +28,7 @@ describe('Input', () => {
             expect(input.__disposers.length).toBe(0)
         })
         it('with value and no options', async () => {
-            const input = new TestInput({value: 'test'})
+            const input = new TestInput({ value: 'test' })
 
             expect(input.value).toBe('test')
             expect(input.options).toBe(undefined)
@@ -37,7 +37,7 @@ describe('Input', () => {
         })
         it('with value and options', async () => {
             const options = TestModel.getQueryX({})
-            const input = new TestInput({value: 'test', options})
+            const input = new TestInput({ value: 'test', options })
 
             expect(input.value).toBe('test')
             expect(input.options).toBe(options)
@@ -45,17 +45,17 @@ describe('Input', () => {
             expect(input.isReady).toBe(false)
             expect(input.__disposers.length).toBe(1)
         })
-         it('with value and options is ready', async () => {
+        it('with value and options is ready', async () => {
             const options = TestModel.getQueryX({})
             runInAction(() => options.__is_ready = true)
-            const input = new TestInput({value: 'test', options})
+            const input = new TestInput({ value: 'test', options })
 
             expect(input.value).toBe('test')
             expect(input.options).toBe(options)
             expect(input.options.isReady).toBe(true)
             expect(input.isReady).toBe(false)
             expect(input.__disposers.length).toBe(1)
-        })  
+        })
     })
 
     describe('isReady', () => {
@@ -64,34 +64,34 @@ describe('Input', () => {
         })
         it('with not ready options ', async () => {
             const options = TestModel.getQueryX({})
-            const input = new TestInput({options})              ; expect(input.isReady).toBe(false)
-            runInAction(() => options.__is_ready = true)        ; expect(input.isReady).toBe(false)
-            input.set('test')                                   ; expect(input.isReady).toBe(true)
+            const input = new TestInput({ options }); expect(input.isReady).toBe(false)
+            runInAction(() => options.__is_ready = true); expect(input.isReady).toBe(false)
+            input.set('test'); expect(input.isReady).toBe(true)
         })
         it('with ready options ', async () => {
             const options = TestModel.getQueryX({})
             runInAction(() => options.__is_ready = true)
 
-            const input = new TestInput({options})              ; expect(input.isReady).toBe(false)
-            input.set('test')                                   ; expect(input.isReady).toBe(true)
-            runInAction(() => options.__is_ready = false)       ; expect(input.isReady).toBe(false)
-            input.set('test')                                   ; expect(input.isReady).toBe(false)
-            runInAction(() => options.__is_ready = false)       ; expect(input.isReady).toBe(false)
-            input.set('test')                                   ; expect(input.isReady).toBe(false)
-            runInAction(() => options.__is_ready = true)        ; expect(input.isReady).toBe(false)
-            input.set('test')                                   ; expect(input.isReady).toBe(true)
+            const input = new TestInput({ options }); expect(input.isReady).toBe(false)
+            input.set('test'); expect(input.isReady).toBe(true)
+            runInAction(() => options.__is_ready = false); expect(input.isReady).toBe(false)
+            input.set('test'); expect(input.isReady).toBe(false)
+            runInAction(() => options.__is_ready = false); expect(input.isReady).toBe(false)
+            input.set('test'); expect(input.isReady).toBe(false)
+            runInAction(() => options.__is_ready = true); expect(input.isReady).toBe(false)
+            input.set('test'); expect(input.isReady).toBe(true)
         })
         it('debounce input inpact to isReady', async () => {
-            const input = new TestInput({debounce: 100})        ; expect(input.isReady).toBe(true)
-            input.set('test')                                   ; expect(input.isReady).toBe(false)
-            input.set('test')                                   ; expect(input.isReady).toBe(false)
-            input.set('test')                                   ; expect(input.isReady).toBe(false)
+            const input = new TestInput({ debounce: 100 }); expect(input.isReady).toBe(true)
+            input.set('test'); expect(input.isReady).toBe(false)
+            input.set('test'); expect(input.isReady).toBe(false)
+            input.set('test'); expect(input.isReady).toBe(false)
             // Fast-forward time
-            jest.runAllTimers()                                 ; expect(input.isReady).toBe(true)
-            input.set('test')                                   ; expect(input.isReady).toBe(false)
-            input.set('test')                                   ; expect(input.isReady).toBe(false)
+            jest.runAllTimers(); expect(input.isReady).toBe(true)
+            input.set('test'); expect(input.isReady).toBe(false)
+            input.set('test'); expect(input.isReady).toBe(false)
             // Fast-forward time
-            jest.runAllTimers()                                 ; expect(input.isReady).toBe(true)
+            jest.runAllTimers(); expect(input.isReady).toBe(true)
         })
     })
 
@@ -102,22 +102,22 @@ describe('Input', () => {
             options,
             autoReset: (i) => i.set('two')
         })
-                                                            expect(input.__disposers.length).toBe(2)
-                                                            expect(input.options.is_ready).toBe(false)
-                                                            expect(input.value).toBe('one')
-        runInAction(() => options.__is_ready = true);       expect(input.value).toBe('two')
-        runInAction(() => options.__is_ready = false);      expect(input.value).toBe('two')
-        input.set('three');                                 expect(input.value).toBe('three')
-        runInAction(() => options.__is_ready = true);       expect(input.value).toBe('two')
-        input.set('three');                                 expect(input.value).toBe('three')
-        runInAction(() => options.__is_ready = true);       expect(input.value).toBe('three')
+        expect(input.__disposers.length).toBe(2)
+        expect(input.options.is_ready).toBe(false)
+        expect(input.value).toBe('one')
+        runInAction(() => options.__is_ready = true); expect(input.value).toBe('two')
+        runInAction(() => options.__is_ready = false); expect(input.value).toBe('two')
+        input.set('three'); expect(input.value).toBe('three')
+        runInAction(() => options.__is_ready = true); expect(input.value).toBe('two')
+        input.set('three'); expect(input.value).toBe('three')
+        runInAction(() => options.__is_ready = true); expect(input.value).toBe('three')
     })
     it('autoReset 2', async () => {
         let test = 0
         const inputA = new StringInput({ syncURLSearchParams: 'inputA' })
         const inputB = new NumberInput({
             syncURLSearchParams: 'inputB',
-            options: TestModel.getQueryX<TestModel>({
+            options: TestModel.getQueryX({
                 filter:
                     XAND(
                         XEQ('eq_a', new BooleanInput({ value: true })),
@@ -127,35 +127,35 @@ describe('Input', () => {
             }),
             autoReset: (i) => { test++ },
         })
-                                                                    expect(test).toBe(0)
-                                                                    expect(inputA.isReady).toBe(true)
-                                                                    expect(inputB.options.isReady).toBe(false)
-                                                                    expect(inputB.isReady).toBe(false)
+        expect(test).toBe(0)
+        expect(inputA.isReady).toBe(true)
+        expect(inputB.options.isReady).toBe(false)
+        expect(inputB.isReady).toBe(false)
         runInAction(() => inputB.options.__is_ready = true)
-                                                                    expect(test).toBe(1)
-                                                                    expect(inputA.isReady).toBe(true)
-                                                                    expect(inputB.options.isReady).toBe(true)
-                                                                    expect(inputB.isReady).toBe(false)
+        expect(test).toBe(1)
+        expect(inputA.isReady).toBe(true)
+        expect(inputB.options.isReady).toBe(true)
+        expect(inputB.isReady).toBe(false)
         inputA.set('test')
-                                                                    expect(test).toBe(1)
-                                                                    expect(inputA.isReady).toBe(true)
-                                                                    expect(inputB.options.isReady).toBe(false)
-                                                                    expect(inputB.isReady).toBe(false)
+        expect(test).toBe(1)
+        expect(inputA.isReady).toBe(true)
+        expect(inputB.options.isReady).toBe(false)
+        expect(inputB.isReady).toBe(false)
         inputB.set(1)
-                                                                    expect(test).toBe(1)
-                                                                    expect(inputA.isReady).toBe(true)
-                                                                    expect(inputB.options.isReady).toBe(false)
-                                                                    expect(inputB.isReady).toBe(false)
+        expect(test).toBe(1)
+        expect(inputA.isReady).toBe(true)
+        expect(inputB.options.isReady).toBe(false)
+        expect(inputB.isReady).toBe(false)
         runInAction(() => inputB.options.__is_ready = true)
-                                                                    expect(test).toBe(2)
-                                                                    expect(inputA.isReady).toBe(true)
-                                                                    expect(inputB.options.isReady).toBe(true)
-                                                                    expect(inputB.isReady).toBe(false)
+        expect(test).toBe(2)
+        expect(inputA.isReady).toBe(true)
+        expect(inputB.options.isReady).toBe(true)
+        expect(inputB.isReady).toBe(false)
         inputB.set(1)
-                                                                    expect(test).toBe(2)
-                                                                    expect(inputA.isReady).toBe(true)
-                                                                    expect(inputB.options.isReady).toBe(true)
-                                                                    expect(inputB.isReady).toBe(true)
+        expect(test).toBe(2)
+        expect(inputA.isReady).toBe(true)
+        expect(inputB.options.isReady).toBe(true)
+        expect(inputB.isReady).toBe(true)
     })
 
     describe('syncLocalStorage', () => {
@@ -173,7 +173,7 @@ describe('Input', () => {
         it('input has value but LocalStorage has more priority', async () => {
             const name = 'test'
             localStorage.setItem(name, 'xxx')
-            const input = new StringInput({value: 'test', syncLocalStorage: name })
+            const input = new StringInput({ value: 'test', syncLocalStorage: name })
             expect(input.value).toBe('xxx')
             expect(localStorage.getItem(name)).toBe('xxx')
         })
